9f45d6fb150a9024ef3a1b433e24da0a37966576
VictorHugo Bisangwa <Viktahugo91@gmail.com>
2023-03-07 18:13:10 +0200

    Counter App Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import react,{useState} from 'react'
+
+
 
 function App() {
+  const [Counter,setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1> Counter App </h1>
+          <div className="my-5">
+             <h2 className="my-5">Count:{Counter}</h2>
+
+             <button 
+                  className="btn btn-success mx-3"
+                  onClick={()=> setCounter(Counter+1) }
+              
+              > 
+                  Increment
+              </button>
+             <button 
+                className="btn btn-danger mx-3"
+                onClick={()=> setCounter(Counter-1)}
+                disabled = {Counter===0}
+              > 
+                   Decrement 
+              </button>
+             <button 
+                className="btn btn-secondary mx-3" 
+                onClick={()=>setCounter(0)}
+                disabled = {Counter===0}
+              > 
+                reset 
+              </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
