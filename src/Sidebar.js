1d10a7c667ea7dca78af3ca01c883a9e3de33348
VictorHugo Bisangwa <Viktahugo91@gmail.com>
2023-03-07 18:26:59 +0200

    Add sidebar Component

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+function Sidebar() {
+    
+    return (
+      <div className="container my-5"> 
+        Name: <h3> Whats You Flavour!</h3> 
+      </div>
+    );
+  }
+  
+  export default Sidebar;
