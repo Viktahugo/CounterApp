f17ac2b4497fb44951ce127454e061b4f664c85a
VictorHugo Bisangwa <Viktahugo91@gmail.com>
2023-03-08 21:02:01 +0200

    Random Jokes

--- src/Randomjoke.js
+++ src/Randomjoke.js
@@ -1,12 +1,29 @@
-import react,{useEffect} from 'react'
+import react,{useEffect, useState} from 'react'
 
 	
 function Randomjoke() {
+    const [joke,setJoke] = useState('');
+    const [icon,setIcon] = useState('');
+    const [id,setId] = useState('');
+    const [updated_at,setUpdated_at]= useState('');
+    const [created_at,setCreated_at] = useState('');
     useEffect( () => {  
-        fetch('https://api.chucknorris.io/jokes/random')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+        const fetchJoke = async () =>
+            await fetch(
+                'https://api.chucknorris.io/jokes/random'
+            ).then(response => response.json())
+	         .then((data) => { 
+                setJoke(data.value);
+                setIcon(data.icon_url);
+                setId(data.id);
+                setUpdated_at(data.updated_at);
+                setCreated_at(data.created_at);
+
+
+                console.log(data);
+        
+        });
+        fetchJoke();
     },[])
     
     return (
@@ -19,9 +36,14 @@ function Randomjoke() {
             <div className="card">
                 <div top width="100%" alt="banner" />
                         <div className="card-body">
-                        <div className=" card-text h3 mb-2 pt-2 font-weight-bold text-secondary">Jokes</div>
+                        <div className=" card-text h3 mb-2 pt-2 font-weight-bold text-secondary">Jokes Called By API</div>
                         <div className="" className="text-secondary mb-4" style={{ fontSize: '0.75rem' }}>
-                           This Field will populate with Jokes!
+                        <p>Id:{id}</p>
+                        <p> <img src={icon} alt="Image not found" /> </p>
+                        <p> the Joke: {joke} </p>
+                        <p>Updated at: {updated_at}</p>
+                        <p> Craeted at: {created_at }</p>
+
                         </div>
                                 
                              <button color="success" className="font-weight-bold">Click To Generate Joke</button>
